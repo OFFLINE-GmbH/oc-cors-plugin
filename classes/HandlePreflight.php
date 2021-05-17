4e9dc5a981678d227d73ec46c4c8c28914fb378c
Tobias Kündig <tobias@offline.ch>
2021-05-17 07:21:47 +0200

    Changed status code to 204 since it has no content

--- classes/HandlePreflight.php
+++ classes/HandlePreflight.php
@@ -33,7 +33,7 @@ class HandlePreflight
             $preflight = $this->cors->handlePreflightRequest($request);
             $response->headers->add($preflight->headers->all());
         }
-        $response->setStatusCode(200);
+        $response->setStatusCode(204);
         return $response;
     }
 
